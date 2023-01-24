1753c2d0bbe93f344cf71035c08ba1dc173b8188
anonymousforp <99302380+anonymousforp@users.noreply.github.com>
2023-01-24 15:51:28 +1100

    Update experiment.py

--- gyms/experiment.py
+++ gyms/experiment.py
@@ -249,7 +249,7 @@ def experiment(
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='hopper')
+    parser.add_argument('--env', type=str, default='TB')
     parser.add_argument('--dataset', type=str, default='medium')  # medium, medium-replay, medium-expert, expert
     parser.add_argument('--mode', type=str, default='normal')  # normal for standard setting, delayed for sparse
     parser.add_argument('--K', type=int, default=20)
